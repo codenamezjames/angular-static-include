0e28ee521f4b726e14b313d185733794289b31a5
jamesharrington <indoisfun@gmail.com>
2014-10-06 16:23:15 -0500

    spacing cleanup

--- includeStatic.js
+++ includeStatic.js
@@ -1,23 +1,23 @@
 angular.module("static-include").directive('includeStatic', function($http, $templateCache, $compile) {
   return {
-  	restrict: 'A',
+    restrict: 'A',
     transclude: true,
     replace: true,
     scope:false,
-  	link: function($scope, element, attrs, ctrl, transclude) {
-  		var templatePath = attrs.includeStatic;
+      link: function($scope, element, attrs, ctrl, transclude) {
+      var templatePath = attrs.includeStatic;
 
-	    try{
-	    	templatePath = $scope.$eval(templatePath);
-	    }catch(err){
-	    	throw new Error(attrs.includeStatic+' is not a valid object');
-	    }
+      try{
+      	templatePath = $scope.$eval(templatePath);
+      }catch(err){
+      	throw new Error(attrs.includeStatic+' is not a valid object');
+      }
 
-	    $http.get(templatePath, { cache: $templateCache })
-	    .success(function(response) {
-	    	var contents = element.html(response).contents();
+      $http.get(templatePath, { cache: $templateCache })
+      .success(function(response) {
+      	var contents = element.html(response).contents();
         $compile(contents)($scope.$parent);
-	    });
-  	}
+      });
+    }
   };
 });
