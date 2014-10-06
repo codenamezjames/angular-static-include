762cf35d61379bf86e16d6598ae272fa67755da2
James Harrington <indoisfun@Gmail.com>
2014-10-06 16:21:19 -0500

    fixed now using parent scope

--- includeStatic.js
+++ includeStatic.js
@@ -1,16 +1,23 @@
 angular.module("static-include").directive('includeStatic', function($http, $templateCache, $compile) {
-    return function($scope, element, attrs) {
-        var templatePath = attrs.includeStatic;
+  return {
+  	restrict: 'A',
+    transclude: true,
+    replace: true,
+    scope:false,
+  	link: function($scope, element, attrs, ctrl, transclude) {
+  		var templatePath = attrs.includeStatic;
 
-        try{
-        	templatePath = $scope.$eval(templatePath);
-        }catch(err){
-        	throw new Error(attrs.includeStatic+' is not a valid object/string');
-        }
+	    try{
+	    	templatePath = $scope.$eval(templatePath);
+	    }catch(err){
+	    	throw new Error(attrs.includeStatic+' is not a valid object');
+	    }
 
-        $http.get(templatePath, { cache: $templateCache }).success(function(response) {
-            var contents = element.html(response).contents();
-            $compile(contents)($scope);
-        });
-    };
+	    $http.get(templatePath, { cache: $templateCache })
+	    .success(function(response) {
+	    	var contents = element.html(response).contents();
+        $compile(contents)($scope.$parent);
+	    });
+  	}
+  };
 });
